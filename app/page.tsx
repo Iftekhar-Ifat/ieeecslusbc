b607a9a0020d0d00332e94460dd5af43ce52656d
Iftekhar-Ifat <iftekharifat007@gmail.com>
2023-11-28 17:48:53 +0600

    added other homepage section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import AchievementSection from "@/components/Homepage/Achievement";
+import ActivitySection from "@/components/Homepage/ActivitySection";
 import HeroSection from "@/components/Homepage/HeroSection";
+import IntroSection from "@/components/Homepage/IntroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <IntroSection />
+      <ActivitySection />
+      <AchievementSection />
     </>
   );
 }
