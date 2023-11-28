33482a853781bc6cbcf4a4d172aa75b75e064673
Iftekhar-Ifat <iftekharifat007@gmail.com>
2023-11-28 17:50:06 +0600

    added use-client for each section

--- components/Homepage/Achievement.tsx
+++ components/Homepage/Achievement.tsx
@@ -1,3 +1,5 @@
+"use-client";
+
 export default function AchievementSection() {
   return <div>AchievementSection</div>;
 }
--- components/Homepage/IntroSection.tsx
+++ components/Homepage/IntroSection.tsx
@@ -1,3 +1,5 @@
+"use-client";
+
 export default function IntroSection() {
   return <div>IntroSection</div>;
 }
