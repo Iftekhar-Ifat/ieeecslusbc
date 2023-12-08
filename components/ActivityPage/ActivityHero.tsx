7eb7f783657823cc1601eb2dc06b35d8b3730ab8
pujan <deb1234.gmail.com>
2023-12-08 20:13:35 +0600

    Activitypage responsiveness added

--- components/ActivityPage/ActivityHero.tsx
+++ components/ActivityPage/ActivityHero.tsx
@@ -9,19 +9,16 @@ export default function ActivityHero() {
   return (
     <div>
       <PageTitle Title={"Our Activities"} image={ActivitypageHeroimage} />
-      <div className="mx-24 mt-6">
+      <div className="sm:mx-0 lg:mx-24 px-4 lg:px-0 mt-6">
         {/* {Research section } */}
         <h3 className="text-xl text-[#045C99] font-bold mb-3 capitalize">Research</h3>
         {/* { cards} */}
-        <div className="flex sm:flex-col md:flex-row justify-between items-center gap-5 mb-4">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-5 mb-4">
           <div className="card w-fit bg-base-100 shadow-xl">
             <figure><Image src={Actvity_1img} alt="" /></figure>
             <div className="card-body">
               <h2 className="card-title">Developing Advanced Security Professionals</h2>
               <p>Digital advanced security stands at the forefront of safeguarding modern technological ecosystems against evolving cyber threats. This multifaceted approach encompasses cutting-edge.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Read more</button>
-              </div>
             </div>
           </div>
 
@@ -30,9 +27,6 @@ export default function ActivityHero() {
             <div className="card-body">
               <h2 className="card-title">Developing Advanced Security Professionals</h2>
               <p>Digital advanced security stands at the forefront of safeguarding modern technological ecosystems against evolving cyber threats. This multifaceted approach encompasses cutting-edge.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Read more</button>
-              </div>
             </div>
           </div>
 
@@ -41,25 +35,22 @@ export default function ActivityHero() {
             <div className="card-body">
               <h2 className="card-title">Developing Advanced Security Professionals</h2>
               <p>Digital advanced security stands at the forefront of safeguarding modern technological ecosystems against evolving cyber threats. This multifaceted approach encompasses cutting-edge.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Read more</button>
-              </div>
             </div>
           </div>
+        </div>
+        <div className="flex justify-end">
+          <button className="btn btn-ghost">See more <ArrowRight /> </button>
         </div>
         <div className="divider"></div>
         {/* {Android section } */}
         <h3 className="text-xl text-[#045C99] font-bold mb-3 capitalize">Android</h3>
         {/* { cards} */}
-        <div className="flex flex-row justify-between items-center gap-5 mb-4">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-5 mb-4">
           <div className="card w-fit bg-base-100 shadow-xl">
             <figure><Image src={Actvity_1img} alt="" /></figure>
             <div className="card-body">
               <h2 className="card-title">Flutter Bootcamp 2022</h2>
               <p>Digital advanced security stands at the forefront of safeguarding modern technological ecosystems against evolving cyber threats. This multifaceted approach encompasses cutting-edge.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-ghost">Read more <ArrowRight /> </button>
-              </div>
             </div>
           </div>
 
@@ -68,9 +59,6 @@ export default function ActivityHero() {
             <div className="card-body">
               <h2 className="card-title">Flutter Bootcamp 2022</h2>
               <p>Digital advanced security stands at the forefront of safeguarding modern technological ecosystems against evolving cyber threats. This multifaceted approach encompasses cutting-edge.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Read more</button>
-              </div>
             </div>
           </div>
 
@@ -79,11 +67,11 @@ export default function ActivityHero() {
             <div className="card-body">
               <h2 className="card-title">Flutter Bootcamp 2022</h2>
               <p>Digital advanced security stands at the forefront of safeguarding modern technological ecosystems against evolving cyber threats. This multifaceted approach encompasses cutting-edge.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Read more</button>
-              </div>
             </div>
           </div>
+        </div>
+        <div className="flex justify-end">
+          <button className="btn btn-ghost">See more <ArrowRight /> </button>
         </div>
       </div>
     </div>
